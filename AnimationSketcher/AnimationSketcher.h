cd5a09808ac439b60399b469dc3ed9a5e7f3590f
shrekshao <shrekshao@gmail.com>
2016-03-07 20:50:22 -0500

    register menu

---
+++ AnimationSketcher/AnimationSketcher.h
@@ -0,0 +1,15 @@
+#pragma once
+
+//#include <maya/MArgList.h>
+//#include <maya/MObject.h>
+//#include <maya/MGlobal.h>
+//#include <maya/MPxCommand.h>
+//
+//class AnimationSketcher : public MPxCommand
+//{
+//public:
+//	AnimationSketcher();
+//	virtual MStatus doIt(const MArgList & argList);
+//
+//	static void *creator();
+//};
